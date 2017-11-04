655263d539fa11339386485bd95442624906a0d9
Sam Holst <sawohol@gmail.com>
2017-11-04 15:56:02 -0600

    added in new js code for image transition

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,11 +1,15 @@
 $(document).on('turbolinks:load', function() {
+
+  var pictures = ["assets/edna_0.jpg", "assets/edna_1.jpg", "assets/edna_2.jpg"];
+  var counter = 1;
+
   if ( $(window).width() < 992 ) {
-    $("#picture-half").css({'background-image': 'url(https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/23120087_1750038968630769_6331254673603386052_o.jpg?oh=70feb8b4450bedfde21eb83f85964241&oe=5A6D0276)', 'background-size': 'cover'});
-    $("#picture-half").height($(window).height() - $('#nav-bar-half').height());
-    $("#picture-half").width($(window).width());
+    // $("#picture-half").css({'background-image': 'url(' + pictures[0] + ')', 'background-size': 'cover'});
+    $("#picture-half img").height($(window).height() - $('#nav-bar-half').height());
+    $("#picture-half img").width($(window).width());
     $("#nav-bar-half").css({'padding-top': $(window).height() * .01}); 
   } else {
-    $("#picture-half").css({'background-image': 'url(https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/23120087_1750038968630769_6331254673603386052_o.jpg?oh=70feb8b4450bedfde21eb83f85964241&oe=5A6D0276)', 'background-size': 'cover'});
+    // $("#picture-half").css({'background-image': 'url(' + pictures[0] + ')', 'background-size': 'cover'});
     $("#picture-half").height($(window).height()- 1);
     $("#picture-half").width($(window).width() - $('#nav-bar-half').width() - 24);
     $("#nav-bar-half").css({'padding-top': $(window).height() * .28}); 
@@ -15,22 +19,37 @@ $(document).on('turbolinks:load', function() {
 
   function adjustPicture(){
     if ( $(window).width() < 992 ) {
-      $("#picture-half").css({'background-image': 'url(https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/23120087_1750038968630769_6331254673603386052_o.jpg?oh=70feb8b4450bedfde21eb83f85964241&oe=5A6D0276)', 'background-size': 'cover'});
+      // $("#picture-half").css({'background-image': 'url(' + pictures[0] + ')', 'background-size': 'cover'});
       $("#picture-half").height($(window).height() - $('#nav-bar-half').height());
       $("#picture-half").width($(window).width());
       $("#nav-bar-half").css({'padding-top': $(window).height() * .01}); 
     } else {
-      $("#picture-half").css({'background-image': 'url(https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/23120087_1750038968630769_6331254673603386052_o.jpg?oh=70feb8b4450bedfde21eb83f85964241&oe=5A6D0276)', 'background-size': 'cover'});
+      // $("#picture-half").css({'background-image': 'url(assets/edna_0.jpg)', 'background-size': 'cover'});
       $("#picture-half").height($(window).height()- 1);
       $("#picture-half").width($(window).width() - $('#nav-bar-half').width() - 24);
       $("#nav-bar-half").css({'padding-top': $(window).height() * .28}); 
     }
   }
 
-  setTimeout(changeBackground, 2000);
+  // setInterval(changeBackground, 5000);
 
   function changeBackground() {
-  
+    if (counter == 0) {
+      $('#picture-half').fadeOut();
+      $("#picture-half").css({'background-image': 'url(' + pictures[counter] + ')', 'background-size': 'cover'});
+      $('#picture-half').fadeIn();
+      counter++;
+    } else if (counter == 1) {
+      $('#picture-half').fadeOut();
+      $("#picture-half").css({'background-image': 'url(' + pictures[counter] + ')', 'background-size': 'cover'});
+      $('#picture-half').fadeIn();
+      counter++;
+    } else {
+      $('#picture-half').fadeOut();
+      $("#picture-half").css({'background-image': 'url(' + pictures[counter] + ')', 'background-size': 'cover'});
+      $('#picture-half').fadeIn();
+      counter = 0;
+    }
   }
 
 });
