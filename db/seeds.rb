58c3005552bb2a8b7d6c22921d5036106e279c32
Sam Holst <sawohol@gmail.com>
2017-11-03 22:53:23 -0600

    fixed cart error with order status by adding the status to the new order upon initialization

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@ Product.delete_all
 Product.create! id: 1, name: "Banana", price: 0.49, active: true
 Product.create! id: 2, name: "Apple", price: 0.29, active: true
 Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+puts "3 products created"
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
 OrderStatus.create! id: 2, name: "Placed"
 OrderStatus.create! id: 3, name: "Shipped"
 OrderStatus.create! id: 4, name: "Cancelled"
+puts "4 order statuses created"
