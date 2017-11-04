6128d09cd7f7f542184eafd4c2ccb6f5a3f84f14
Sam Holst <sawohol@gmail.com>
2017-11-03 19:31:08 -0600

    added controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/contact'
   get 'pages/about'
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
 end
