3f67518d820793131a2b45ef921134b596bc7c94
Sam Holst <sawohol@gmail.com>
2017-11-05 23:34:07 -0700

    added new css and changed styles

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $("#empty-cart").css({'padding-top': ($(window).height() * .2)}); 
+  // $("#empty-cart").css({'padding-top': ($(window).height() * .2)}); 
 });
--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,14 +1,14 @@
 // $(document).on('turbolinks:load', function() {
 $(document).ready(function(){
-  if ( $(window).width() < 992 ) {
+  if ( $(window).width() < 1200 ) {
     $("div#nav-bar-half").insertAfter( $("div#picture-half") );
     $("#picture-half img").width($(window).width());
     $("#picture-half").css({"overflow": "hidden"});
     $("#picture-half").height($(window).height() * .5);
     $("#nav-bar-half").css({'padding-top': $(window).height() * .01}); 
   } else {
-    $("#picture-half img").height($(window).height() - 1);
-    $("#picture-half img").width($(window).width() - $('#nav-bar-half').width() - 32);
+    $("#picture-half img").height($(window).height());
+    $("#picture-half img").width($(window).width() - $('#nav-bar-half').width() - 30);
     $("#picture-half").css({"height": "auto"});
     $("#nav-bar-half").css({'padding-top': $(window).height() * .28}); 
   }
@@ -16,16 +16,16 @@ $(document).ready(function(){
   window.addEventListener("resize", adjustPicture);
 
   function adjustPicture(){
-    if ( $(window).width() < 992 ) {
+    if ( $(window).width() < 1200 ) {
+      $("div#nav-bar-half").insertAfter( $("div#picture-half"));
       $("#picture-half img").css({"width": "100%"});
       $("#picture-half").height($(window).height() * .5);
       $("#picture-half").css({"overflow": "hidden"});
-      $("div#nav-bar-half").insertAfter( $("div#picture-half") );
       $("#nav-bar-half").css({'padding-top': $(window).height() * .01}); 
     } else {
       $("div#picture-half").insertAfter( $("div#nav-bar-half") );
-      $("#picture-half img").height($(window).height() - 1);
-      $("#picture-half").height($(window).height() - 1);
+      $("#picture-half img").height($(window).height());
+      $("#picture-half").height($(window).height());
       $("#nav-bar-half").css({'padding-top': $(window).height() * .28}); 
     }
   }
